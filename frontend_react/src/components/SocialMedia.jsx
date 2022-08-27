61849b1abf7c92356e836ad7db2157f5526bb0c0
Hermansyah Putra <hermansyahputra0418@gmail.com>
2022-08-27 22:32:07 +0700

    Perbaiki SocialMedia Section

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -6,15 +6,19 @@ const SocialMedia = () => {
   return (
     <div className="app__social">
       <div>
-        <a href="https://github.com/putraa22" target="_blank">
+        <a href="https://github.com/putraa22" target="_blank" rel="noreferrer">
           <BsGithub />
         </a>
       </div>
       <div>
-        <FaFacebookF />
+        <a href="https://web.facebook.com/Putraaa22/" target="_blank" rel="noreferrer">
+          <FaFacebookF />
+        </a>
       </div>
       <div>
-        <BsInstagram />
+        <a href="#" rel="noreferrer">
+          <BsInstagram />
+        </a>
       </div>
     </div>
   );
